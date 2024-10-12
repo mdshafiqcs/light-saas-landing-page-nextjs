641e6a89438f2b3145e5b964bd23dac5271cfa99
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-12 09:20:49 +0600

    tailwind.config.ts updated, added xl screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
       sm: "375px",
       md: "768px",
       lg: "1200px",
-      
+      xl: "1536px",
     },
     extend: {
       container: {
@@ -20,7 +20,7 @@ const config: Config = {
         padding: {
           DEFAULT: "20px",
           lg: "80px",
-          xl: "50px"
+          xl: "100px",
         },
       },
     },
