e1d14d9f158e5c364e3d365ec1917580495ae92a
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-13 11:58:11 +0600

    product showcase animation

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import { pyramidImage, tubeImage, productImage } from "@/assets";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 
 export const ProductShowcase = () => {
-  return <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
+
+  const sectionRef =  useRef(null);
+
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150, -150]);
+
+  return <section ref={sectionRef} className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
     <div className="container">
         <div className="section-heading">
           <div className='flex justify-center'>
@@ -14,8 +28,26 @@ export const ProductShowcase = () => {
         </div>
       <div className='relative'>
       <Image src={productImage} alt="product image" className="mt-10" />
-        <Image src={pyramidImage} width={262} height={262} alt="pyramid image" className="hidden md:block absolute -top-32 -right-36" />
-        <Image src={tubeImage} height={248} alt="tube image" className='hidden md:block absolute bottom-24 -left-36' />
+        <motion.img 
+        src={pyramidImage.src} 
+        width={262} 
+        height={262} 
+        alt="pyramid image" 
+        className="hidden md:block absolute -top-32 -right-36"
+        style={{
+          translateY,
+        }} 
+        />
+        <motion.img 
+        src={tubeImage.src} 
+        height={248} 
+        width={248} 
+        alt="tube image" 
+        className='hidden md:block absolute bottom-24 -left-36'
+        style={{
+          translateY,
+        }} 
+        />
       </div>
     </div>
   </section>
