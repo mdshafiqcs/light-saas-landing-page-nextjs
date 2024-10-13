cd42659e99f136cd517b3e7b7ea31d4454dcf603
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-13 15:42:12 +0600

    call to action animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,44 @@
+"use client";
+
 import { ArrowRight, starImage, springImage } from "@/assets";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 
 export const CallToAction = () => {
-  return <section className="py-24 bg-gradient-to-b from-white to-[#D2DCFF] overflow-x-clip ">
+
+  const sectionRef = useRef(null);
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150, -150]) ;
+
+  return <section ref={sectionRef} className="py-24 bg-gradient-to-b from-white to-[#D2DCFF] overflow-x-clip ">
     <div className="container">
       <div className='section-heading relative'>
         <h2 className='section-title'>Sign up for free today</h2>
         <p className="section-description mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivated your efforts.</p>
-        <Image src={starImage} width={360} alt="star image" className="hidden md:block absolute -left-[350px] -top-[137px]" />
-        <Image src={springImage} width={360} alt="spring image" className="hidden md:block absolute -right-[331px] -top-[19px]" />
+        <motion.img 
+        src={starImage.src} 
+        width={360} 
+        alt="star image" 
+        className="hidden md:block absolute -left-[350px] -top-[137px]" 
+        style={{
+          translateY,
+        }}
+        />
+        <motion.img 
+        src={springImage.src}
+        width={360} 
+        alt="spring image" 
+        className="hidden md:block absolute -right-[331px] -top-[19px]" 
+        style={{
+          translateY,
+        }}
+        />
       </div>
       <div className="flex justify-center mt-10 gap-2">
         <button className='btn btn-primary'>Get for free</button>
