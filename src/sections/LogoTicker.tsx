98812b4e6f6ed023af8538e1f3e99dec2c38f474
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-15 10:12:53 +0600

    fixed logo ticker animation issue

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -4,34 +4,43 @@ import Image from "next/image";
 import { acmeLogo, apexLogo, echoLogo, pulseLogo, quantumLogo, celestialLogo } from "@/assets";
 import { motion } from "framer-motion";
 
+const images = [
+  { src: acmeLogo, alt: "Acme Logo" },
+  { src: quantumLogo, alt: "Quantum Logo" },
+  { src: echoLogo, alt: "Echo Logo" },
+  { src: celestialLogo, alt: "Celestial Logo" },
+  { src: pulseLogo, alt: "Pulse Logo" },
+  { src: apexLogo, alt: "Apex Logo" },
+];
+
 export const LogoTicker = () => {
   return <section className='py-8 md:py-12 bg-white'>
     <div className='container'>
-    <div className='overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] '>
+    <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] '>
       <motion.div 
-      className="flex gap-14 flex-none"
+      className="flex gap-14 flex-none pr-14"
+      initial={{ translateX: 0 }}
       animate={{
         translateX: "-50%"
       }}
       transition={{
         duration: 20,
         repeat: Infinity,
-        repeatType: "loop",
         ease: "linear"
       }}
       >
-        <Image src={acmeLogo} alt="Acme Logo" className='logo-ticker-image'/>
-        <Image src={apexLogo} alt="Apex Logo" className='logo-ticker-image' />
-        <Image src={echoLogo} alt="Echo Logo" className='logo-ticker-image'/>
-        <Image src={pulseLogo} alt="Pulse Logo" className='logo-ticker-image'/>
-        <Image src={quantumLogo} alt="Quantum Logo" className='logo-ticker-image'/>
-        <Image src={celestialLogo} alt="Celestial Logo" className='logo-ticker-image'/>
-        <Image src={acmeLogo} alt="Acme Logo" className='logo-ticker-image'/>
-        <Image src={apexLogo} alt="Apex Logo" className='logo-ticker-image' />
-        <Image src={echoLogo} alt="Echo Logo" className='logo-ticker-image'/>
-        <Image src={pulseLogo} alt="Pulse Logo" className='logo-ticker-image'/>
-        <Image src={quantumLogo} alt="Quantum Logo" className='logo-ticker-image'/>
-        <Image src={celestialLogo} alt="Celestial Logo" className='logo-ticker-image'/>
+
+        {
+          images.concat(images).map(({src, alt}) => (
+            <Image src={src} alt={alt} key={src.src} className='logo-ticker-image flex-none'/>
+          ))
+        }
+        {
+          images.concat(images).map(({src, alt}) => (
+            <Image src={src} alt={alt} key={src.src} className='logo-ticker-image flex-none'/>
+          ))
+        }
+
       </motion.div>
     </div>
     </div>
